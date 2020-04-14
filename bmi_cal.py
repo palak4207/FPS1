6327d35042a23ca6e1c34dcd77c20c5ebd9a9b5c
palak@4207 <palak4207@gmail.com>
2020-04-14 15:46:38 +0530

    Adding

---
+++ bmi_cal.py
@@ -0,0 +1,12 @@
+"""
+Assignment-1
+BMI Calculator
+By Palak Agrawal Mandsaur (M.P.)
+"""
+
+
+height= float(input("enter your height in meter"))
+weight= float(input("enter your Weight in Kilogram"))
+bmi=weight/(height**2)
+print("Your BMI weight is:")
+print(bmi)
